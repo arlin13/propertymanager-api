b12d33a3b33b066300dc067aadaa9edd71f80721
Arlin Grijalba <arlin.grijalba@gmail.com>
2017-04-29 23:32:08 -0700

    Implementation of BaseApiController

--- PropertyManagement.API/Utility/BaseApiController.cs
+++ PropertyManagement.API/Utility/BaseApiController.cs
@@ -1,13 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using PropertyManagement.API.Data;
+using PropertyManagement.API.Models;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace PropertyManagement.API.Utility
 {
     public class BaseApiController : ApiController
     {
+        protected PropertyManagementDataContext db = new PropertyManagementDataContext();
+
+        public User CurrentUser
+        {
+            get
+            {
+                var userName = base.User.Identity.Name;
+                var user = db.Users.FirstOrDefault(u => u.UserName == userName);
+
+                return user;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
     }
 }
